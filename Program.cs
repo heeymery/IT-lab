6082ec95a692ce041a54b55c5220f6a66600a350
heeymery <marishkahesenli@gmail.com>
2025-03-05 22:25:32 +0400

    test

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int eded;
+       eded= int.Parse(Console.ReadLine());
+        if (eded > 0) {
+            Console.WriteLine("eded musbetdir.");
+        }
+        else if(eded < 0)
+        {
+            Console.WriteLine("eded menfidir.");
+        }
+        else
+        {
+            Console.WriteLine("eded sifirdir.");
+        }
+    }
+}
